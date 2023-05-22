8cbf8910d6e9b541179b2f1b5e91531cbf7f2aa8
Adam-IT <Alan_Salons@mail.ru>
2023-05-22 13:19:42 +0300

    создал проект, содержащий импорт и экспорт

---
+++ first2/first.js
@@ -0,0 +1,8 @@
+export let a = 45;
+export let b = 35;
+export let s = 'Lorem'
+function summ() {
+  return a + b;
+}
+
+export default summ;
---
+++ first2/second.js
@@ -0,0 +1,4 @@
+import { a, b, s } from "./first.js";
+import summ from "./first.js";
+console.log(a, b, s);
+console.log(summ());
